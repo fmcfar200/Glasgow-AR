0ea7c9d0d496c43e205750f5844ccdda7ee762ca
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-09-28 16:53:48 +0100

    Init commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int currentHealth;
+    int maxHealth = 100;
+    bool alive = true;
+
+    Animator animator;
+
+	void Start()
+    {
+        animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+        
+    }
+
+    void Death()
+    {
+        animator.SetInteger("Dead", 1);
+        alive = false;
+    }
+
+    IEnumerator WaitandDestroy(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(this.gameObject);
+
+    }
+}
---
+++ Assets/Scripts/PlayerCombatScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombatScript : MonoBehaviour {
+
+    RaycastHit hit;
+    Vector2[] touches = new Vector2[5];
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.touchCount > 0)
+        {
+            foreach(Touch t in Input.touches)
+            {
+                touches[t.fingerId] = Camera.main.ScreenToWorldPoint(Input.GetTouch(t.fingerId).position);
+                if (Input.GetTouch(t.fingerId).phase == TouchPhase.Began)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(
+                        Input.GetTouch(t.fingerId).position);
+                    if (Physics.Raycast(ray,out hit))
+                    {
+                        if (hit.collider.gameObject.tag == "enemy")
+                        {
+
+                            hit.collider.GetComponent<Enemy>().currentHealth -= 25;
+                        }
+                    }
+                }
+                   
+                
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                
+                if (Physics.Raycast(ray,out hit))
+                {
+                    if (hit.collider.gameObject.tag == "enemy")
+                    {
+                        print("hit");
+                        hit.collider.GetComponent<Enemy>().currentHealth -= 25;
+                    }
+                }
+                    
+
+                
+            }
+        }
+	}
+}
