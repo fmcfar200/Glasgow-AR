ec385537969b5660d225fc661c2b8b417bbaa1da
fmcfar200 <fmcfar200@caledonian.ac.uk>
2018-01-31 20:04:40 +0000

    map test

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -2,7 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Vuforia;
+
 public class GameManagerScript : MonoBehaviour {
+
+
+	void Start()
+	{
+		Scene scene = SceneManager.GetActiveScene ();
+
+		if (scene.name == "AR Scene") {
+
+			Camera.main.GetComponent<VuforiaBehaviour> ().enabled = true;
+		} 
+		else 
+		{
+			Camera.main.GetComponent<VuforiaBehaviour> ().enabled = false;
+		}	
+
+	}
 
 	public void ReloadScene()
     {
@@ -13,4 +31,5 @@ public class GameManagerScript : MonoBehaviour {
     {
         SceneManager.LoadScene(name);
     }
+
 }
