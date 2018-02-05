4363cd03893c4eb5c72631c8aca7b56f7ed1e8ce
fmcfar200 <fmcfar200@caledonian.ac.uk>
2018-02-05 18:44:59 +0000

    GPS Start

---
+++ Assets/GPSLocation.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPSLocation : MonoBehaviour {
+
+    public float lat, lon;
+    public Text infoText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //starts test on init
+        StartCoroutine(TestLocServices());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		while(Input.location.status == LocationServiceStatus.Running)
+        {
+            lat = Input.location.lastData.latitude;
+            lon = Input.location.lastData.longitude;
+
+            infoText.text = "Location: " + lat + "  " + lon;
+        }
+
+        
+	}
+
+    IEnumerator TestLocServices()
+    {
+        //is service enabled?
+        if (!Input.location.isEnabledByUser)
+            yield break;
+       
+        //Start Service
+        Input.location.Start();
+
+        //wait for init
+        int maxWait = 15;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        //timed out
+        if (maxWait < 1)
+        {
+            Debug.LogError("Location Services Timed Out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.LogError("Location Service Failed");
+            yield break;
+        }
+        
+    }
+}
