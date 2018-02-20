8cab0f489a10de94c79b104e1e9ddd0eb9d9793c
fmcfar200 <fmcfar200@caledonian.ac.uk>
2018-02-20 15:49:05 +0000

    Map Spawn Test

---
+++ Assets/MapManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+using Mapbox.Unity.Utilities;
+
+
+public class MapManager : MonoBehaviour {
+
+
+    public AbstractMap map;
+    private GameObject player;
+
+    private float pLat, pLon;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("PlayerIcon");
+
+        if (player == null)
+        {
+            Debug.LogError("Player Object not found!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (player != null && map != null)
+        {
+            pLat = player.GetComponent<GPSLocation>().lat;
+            pLon = player.GetComponent<GPSLocation>().lon;
+            Vector2d enemyLatLon = new Vector2d((double)pLat, (double)pLon);
+            if (Input.GetMouseButtonDown(0)
+                || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Debug.Log("click");
+                Vector3 enemyIconPos = Conversions.GeoToWorldPosition(enemyLatLon, map.CenterMercator, map.WorldRelativeScale).ToVector3xz();
+                GameObject testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                testObject.transform.position = enemyIconPos;
+            }
+        }
+
+        
+    }
+}
