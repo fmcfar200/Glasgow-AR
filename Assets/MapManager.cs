b86cdd4735090fed4bdeae4e3a332e75a704c74d
fmcfar200 <fmcfar200@caledonian.ac.uk>
2018-02-20 17:25:23 +0000

    Adding some spawning mechanics

--- Assets/MapManager.cs
+++ Assets/MapManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 using Mapbox.Unity.Map;
 using Mapbox.Utils;
@@ -9,41 +10,60 @@ using Mapbox.Unity.Utilities;
 
 public class MapManager : MonoBehaviour {
 
-
     public AbstractMap map;
-    private GameObject player;
+    public GameObject player;
 
     private float pLat, pLon;
+
+    public int enemyAmount = 0;
+    private GameObject gameManager;
+
+
 
 	// Use this for initialization
 	void Start ()
     {
-        player = GameObject.FindGameObjectWithTag("PlayerIcon");
 
+        player = GameObject.FindGameObjectWithTag("PlayerIcon");
+        gameManager = GameObject.FindGameObjectWithTag("Game Manager");
         if (player == null)
         {
             Debug.LogError("Player Object not found!");
         }
+        
+
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-		if (player != null && map != null)
-        {
-            pLat = player.GetComponent<GPSLocation>().lat;
-            pLon = player.GetComponent<GPSLocation>().lon;
-            Vector2d enemyLatLon = new Vector2d((double)pLat, (double)pLon);
-            if (Input.GetMouseButtonDown(0)
-                || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-            {
+        
+      if (player != null && map != null)
+      {
+          pLat = player.GetComponent<GPSLocation>().lat;
+          pLon = player.GetComponent<GPSLocation>().lon;
+          Vector2d enemyLatLon = new Vector2d((double)pLat, (double)pLon);
+          if (Input.GetMouseButtonDown(0)
+              || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+          {
                 Debug.Log("click");
                 Vector3 enemyIconPos = Conversions.GeoToWorldPosition(enemyLatLon, map.CenterMercator, map.WorldRelativeScale).ToVector3xz();
+                
+                float randomX, randomZ;
+                randomX = Random.Range(-2.6f, 2.6f);
+                randomZ = Random.Range(-2.6f, 2.6f);
+                enemyIconPos.x += randomX;
+                enemyIconPos.z += randomZ;
+                
                 GameObject testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 testObject.transform.position = enemyIconPos;
-            }
-        }
+                
+                enemyAmount++;
+                gameManager.GetComponent<GameManagerScript>().enemyAmount = enemyAmount;
+          
+          }
+      }
 
-        
-    }
-}
+     }
+ }
+
--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -6,14 +6,30 @@ using Vuforia;
 
 public class GameManagerScript : MonoBehaviour {
 
+    public static GameManagerScript manager;
+    public GameObject testPrefab;
+    public int enemyAmount = 0;
+
 
 	void Start()
 	{
+
+        if (manager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            manager = this;
+        }
+        else if (manager != this)
+        {
+            Destroy(gameObject);
+        }
+
 		Scene scene = SceneManager.GetActiveScene ();
 
 		if (scene.name == "AR Scene") {
 
 			Camera.main.GetComponent<VuforiaBehaviour> ().enabled = true;
+        
 		} 
 		else 
 		{
