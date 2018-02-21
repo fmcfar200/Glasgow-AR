fac067d43c7fc84ea814673be32d4552a261baa3
fmcfar200 <fmcfar200@caledonian.ac.uk>
2018-02-21 15:16:50 +0000

    Spawn on updated location

--- Assets/MapManager.cs
+++ Assets/MapManager.cs
@@ -8,6 +8,8 @@ using Vuforia;
 using Mapbox.Unity.Map;
 using Mapbox.Utils;
 using Mapbox.Unity.Utilities;
+using Mapbox.Unity.Location;
+
 
 
 public class MapManager : MonoBehaviour {
@@ -20,10 +22,22 @@ public class MapManager : MonoBehaviour {
     public int enemyAmount = 0;
     private GameObject gameManager;
 
+    ILocationProvider _locationProvider;
+    ILocationProvider LocationProvider
+    {
+        get
+        {
+            if (_locationProvider == null)
+            {
+                _locationProvider = LocationProviderFactory.Instance.DefaultLocationProvider;
+            }
 
+            return _locationProvider;
+        }
+    }
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
 
         player = GameObject.FindGameObjectWithTag("PlayerIcon");
@@ -46,6 +60,14 @@ public class MapManager : MonoBehaviour {
             Camera.main.GetComponent<VuforiaBehaviour>().enabled = false;
         }
 
+        LocationProvider.OnLocationUpdated += LocationProvider_OnLocationUpdated;
+
+    }
+    void LocationProvider_OnLocationUpdated(object sender, LocationUpdatedEventArgs e)
+    {
+        LocationProvider.OnLocationUpdated -= LocationProvider_OnLocationUpdated;
+        SpawnEnemyIcons();
+        
     }
 
     // Update is called once per frame
@@ -56,28 +78,39 @@ public class MapManager : MonoBehaviour {
       {
           pLat = player.GetComponent<GPSLocation>().lat;
           pLon = player.GetComponent<GPSLocation>().lon;
-          Vector2d enemyLatLon = new Vector2d((double)pLat, (double)pLon);
-          if (Input.GetMouseButtonDown(0)
-              || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-          {
-                Debug.Log("click");
-                Vector3 enemyIconPos = Conversions.GeoToWorldPosition(enemyLatLon, map.CenterMercator, map.WorldRelativeScale).ToVector3xz();
-                
-                float randomX, randomZ;
-                randomX = Random.Range(-2.6f, 2.6f);
-                randomZ = Random.Range(-2.6f, 2.6f);
-                enemyIconPos.x += randomX;
-                enemyIconPos.z += randomZ;
-                
-                GameObject testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                testObject.transform.position = enemyIconPos;
-                
-                enemyAmount++;
-                gameManager.GetComponent<GameManagerScript>().enemyAmount = enemyAmount;
           
-          }
       }
 
      }
+
+    void SpawnEnemyIcons()
+    {
+        Debug.Log("Location Update");
+
+        Vector2d enemyLatLon = new Vector2d((double)pLat, (double)pLon);
+        int spawnAmount = Random.Range(1, 4);
+
+        for(int i = 0; i < spawnAmount; i++)
+        {
+            Vector3 enemyIconPos = Conversions.GeoToWorldPosition(enemyLatLon, map.CenterMercator, map.WorldRelativeScale).ToVector3xz();
+
+            float randomX, randomZ;
+            randomX = Random.Range(-2.6f, 2.6f);
+            randomZ = Random.Range(-2.6f, 2.6f);
+            enemyIconPos.x += randomX;
+            enemyIconPos.z += randomZ;
+
+            GameObject testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            testObject.tag = "Enemy Icon";
+            testObject.transform.position = enemyIconPos;
+
+            enemyAmount++;
+        }
+
+        
+        gameManager.GetComponent<GameManagerScript>().enemyAmount = enemyAmount;
+
+    }
+    
  }
 
